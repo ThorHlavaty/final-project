b9c6e29f4ec0896a2f00e63052d6cef210f93eda
Thor Hlavaty <thorthebore@gmail.com>
2020-11-25 15:42:24 -0500

    order page basic layout complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import Login from './Login';
 import {Link, Route, Switch} from 'react-router-dom'
 import Dashboard from './Dashboard';
+import Order from './Components/Order/Order';
 
 function App() {
   return (
     <>
-      <Link to='/users/register'>Register</Link>
+      {/* <Link to='/users/register'>Register</Link>
       <Link to='/users/login'>Login</Link>
       <Link to='/users/dashboard'>Dashboard</Link>
+      <Link to='/table/id'>Table Test page</Link> */}
       <Switch>
         <Route exact path='/'/>
         <Route exact path='/users/register' component={Registration}/>
         <Route exact path='/users/login' component={Login}/>
         <Route exact path='/users/dashboard' component={Dashboard}/>
+        <Route exact path='/table/id' component={Order}/>
+
       </Switch>
     </>
 
