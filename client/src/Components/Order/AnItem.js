180661e150172c3dd85b4933213502734ba9cd4e
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-12-01 17:18:11 -0500

    highlight color and bottom border

--- client/src/Components/Order/AnItem.js
+++ client/src/Components/Order/AnItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
+import './Guest.css'
 
 export default function AnItem(props) {
   return (
-    <div>
+    <div className='guest'>
     <Grid divided style={{fontSize:'12px'}}>
     <Grid.Column width={8}  style={{fontSize:'12px'}}>
       {props.shorthand}
