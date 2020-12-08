e202e7c41b5043ebf04a8d75d9bbe86abb973191
Thor Hlavaty <thorthebore@gmail.com>
2020-12-08 16:25:55 -0500

    aes·thet·ic

--- client/src/pages/MobileTable/MobileTableNav/MobileTableNav.js
+++ client/src/pages/MobileTable/MobileTableNav/MobileTableNav.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react'
 import './MobileTableNav.css'
 import axios from 'axios'
 import SignOutBar from '../../../Components/SignOutBar/SignOutBar'
+import { useSelector } from 'react-redux'
 
 
 export default function MobileTableNav(props) {
 
     const [ sectionData, setSectionData ] = useState()
+    const serverId = useSelector(state=>state.userSignin.userInfo.id)
+    const manager = useSelector(state=>state.userSignin.userInfo.manager)
+
 
     useEffect(() => {
         axios.get('/api/v1/section')
@@ -27,7 +31,7 @@ export default function MobileTableNav(props) {
             <ul className='mobileTableNav--list'>
                {(sectionData !== undefined) && sectionData.map((sections)=>
                    (<li>
-                        <button className='mobileTableNav--btn' onClick={()=>props.setSection(sections.id)}>Section {sections.id}</button>
+                        <button className='mobileTableNav--btn' onClick={()=> ((serverId === sections.id || manager) ? props.setSection(sections.id): alert('This is not your section!'))}>Section {sections.id}</button>
                     </li>)
                )}
             <div className='bar' >
